718c01dbb62b0d3afb801a24e8b1344a72d2ad50
NikolGraminova <nbackup619@gmail.com>
2023-07-06 14:54:10 +0200

    Created main function

--- js/main.js
+++ js/main.js
@@ -2,10 +2,32 @@
 
 Milestone 1:
 Crea una funzione chiamata sommaNumeri che prenda un array di N numeri e calcoli la somma di tutti questi numeri contenuti nell'array, restituendolo come risultato.
-Utilizzare la funzione sommaNumeri per stampare il risultato. L'array di N numeri da passare alla funzione lo create chiedendo all'utente quanti numeri vuole inserire (cioè N), e poi facendogli inserire questi N numeri all'utente stesso tramite apposito prompt().
+Utilizzare la funzione sommaNumeri per stampare il risultato. L'array di N numeri da passare alla funzione lo create chiedendo all'utente quanti numeri vuole inserire (cioè N), 
+e poi facendogli inserire questi N numeri all'utente stesso tramite apposito prompt().
 
 Milestone 2:
 Crea una funzione chiamata mediaAritmetica che prende in input un array di N numeri e restituisca la media aritmetica dei numeri contenuti nell'array.
 Utilizzare la funzione sommaNumeri precedentemente creata per calcolare la media dei numeri che vanno chiesti all'utente attraverso dei prompt.
 Potete ad esempio provare ad usare questo programma per inserire N voti di scuola e farvi restituire qual è la media di tutti i voti inseriti! */
 
+
+// ----- Programma principale -----
+
+let numbersAmount = parseInt(prompt("Quanti numeri vuoi inserire? :"));
+let numberUser = parseInt(prompt("Aggiungi un numero:"))
+
+console.log(sommaNumeri(numbersAmount, numberUser));
+
+
+
+// ----- Funzioni -----
+
+function sommaNumeri(arrayLength, num) {
+    let result = 0;
+    let numbersArray = [];
+    for (let i = 0; i < arrayLength; i++) {
+        numbersArray.push(num);
+        result = + num;
+    } return result;
+
+}
